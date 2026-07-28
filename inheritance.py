0bc5ea0d1c67b2d0f380b0c3e864e7c4dc41b23a
shrikrushnafasale44-ops <shrikrushnafasale44@gmail.com>
2026-07-28 10:12:51 +0530

    Add Student and College classes with display methods

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Student:
+    def show(self):
+        print("Name: Shrikrushna")
+
+class College(Student):
+    def display(self):
+        print("College: MIT ADT")
+
+c = College()
+c.show()
+c.display()
